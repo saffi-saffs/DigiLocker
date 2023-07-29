373d4b5a0d514196447962aa42ab50061aa57e21
momopoodle <poudelmohini@gmail.com>
2023-07-29 07:13:25 +0545

    redirect to files instead of dashboard after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/files';
 
     /**
      * Create a new controller instance.
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,7 +98,6 @@
           </ul>
           <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-            
               <li class="nav-item dropdown">
                 <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
                   aria-expanded="false">
